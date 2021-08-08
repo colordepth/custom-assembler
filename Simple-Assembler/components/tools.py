122fedb159324e0e10c6099074a3836a2a7b3205
Deep <deepsharma.cs@gmail.com>
2021-08-08 17:49:14 +0530

    update removeLabel function
    bugFix on line 18 when None was being returned

--- Simple-Assembler/components/tools.py
+++ Simple-Assembler/components/tools.py
@@ -12,8 +12,10 @@
 ########################################
 
 def removeLabel(asm_string):
+	# INPUT: 	"labelName: mov R1 R2 R3"
+	# OUTPUT: 	"mov R1 R2 R3"
 	if ':' not in asm_string:
-		return
+		return asm_string
 	
 	asm_string = asm_string[asm_string.find(':')+1:]
 	asm_string = asm_string.lstrip()
@@ -23,4 +25,6 @@ def removeLabel(asm_string):
 def convertToBin(immediate):
 	# Returns binary representation of immediate
 	# Throw error if immediate is not 8 bit
+	# INPUT: 	$5
+	# OUTPUT: 	"00000101"
 	pass
