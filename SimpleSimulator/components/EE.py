01789e9b3f9f74608679573cb0ed2e2f5ba60e19
Karan Singh <karan20383@iiitd.ac.in>
2021-08-14 03:55:20 +0530

    update EE
    solve resolution issues
    move updation before function call to accommodate branching
    improve readability
    add headers

--- SimpleSimulator/components/EE.py
+++ SimpleSimulator/components/EE.py
@@ -1,11 +1,30 @@
+########################################
+
+# AUTHORS (Group 20):
+
+# Deep Sharma 2020370
+# Dhruv Malik 2020373
+# Karan Singh 2020383
+
+
+# Execution unit
+
+# calls required functions for execution
+
+# INPUT: instruction
+# OUTPUT: NONE 
+########################################
+
 from .shared import *
-from .PC import *
+import components.PC
 def opcodeExtract(instruction):
     return instruction[0:5]
 
 def execute(instruction):
+    update(components.PC.PC+1)
+
     opcode=opcodeExtract(instruction)
-    functions[opcode](types[opcode](instruction))
-    update(PC+1)
-#branchstatement
+    operands=types[opcode](instruction)
+    
+    functions[opcode](operands)
 
