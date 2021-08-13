3f2436f2f5eba04d3da55ecfc4b46b3c1208b881
Karan Singh <karan20383@iiitd.ac.in>
2021-08-14 03:56:52 +0530

    update PC
    solve resolution error
    add header

--- SimpleSimulator/components/PC.py
+++ SimpleSimulator/components/PC.py
@@ -1,4 +1,22 @@
-from .shared import *
+########################################
+
+# AUTHORS (Group 20):
+
+# Deep Sharma 2020370
+# Dhruv Malik 2020373
+# Karan Singh 2020383
+
+
+# Program Counter
+
+# Progra counter and related functions
+
+# INPUT: NONE/(New program counter)
+# OUTPUT:  NONE/(value of program counter)
+########################################
+
+import components.shared
+
 PC=0
 
 def update(new_PC):
@@ -6,4 +24,4 @@ def update(new_PC):
     PC=new_PC
 
 def dump():
-    print(convertToBinary8(PC),end=" ")
+    print(components.shared.convertToBinary8(PC),end=" ")
