1f88e296b76aa9ed287dfe04bc77f4ee01e60571
Karan Singh <karan20383@iiitd.ac.in>
2021-08-14 03:57:39 +0530

    update RF
    change flag register to a string for easy handling
    add header

--- SimpleSimulator/components/RF.py
+++ SimpleSimulator/components/RF.py
@@ -1,18 +1,35 @@
+########################################
+
+# AUTHORS (Group 20):
+
+# Deep Sharma 2020370
+# Dhruv Malik 2020373
+# Karan Singh 2020383
+
+
+# Register FIle
+
+# contains registers and realted function
+
+# INPUT: NONE
+# OUTPUT: register values
+########################################
+ 
 import components.shared
 
-register_value={     \
-    "000" : 0,       \
-    "001" : 0,       \
-    "010" : 0,       \
-    "011" : 0,       \
-    "100" : 0,       \
-    "101" : 0,       \
-    "110" : 0,       \
-    "111" : 0        \
+register_value={         \
+    "000" : 0,           \
+    "001" : 0,           \
+    "010" : 0,           \
+    "011" : 0,           \
+    "100" : 0,           \
+    "101" : 0,           \
+    "110" : 0,           \
+    "111" : "0"*16       \
 }
 
-##possible issue unordered output
 def dump():
     for value in sorted(register_value.keys()):
-        print(components.shared.convertToBinary16(register_value[value]),end=" ")
-    print("")
+        if value!="111":
+            print(components.shared.convertToBinary16(register_value[value]),end=" ")
+    print(register_value[value])
