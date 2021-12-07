a129b72f3e85c2e3ea621f10facf296c0da8c2f7
Deep Sharma <deepsharma.cs@gmail.com>
2021-12-08 03:58:32 +0530

    add further logging of HTTP requests

--- server/app.js
+++ server/app.js
@@ -5,12 +5,11 @@ const assemblerRouter = require('./controllers/assembler')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
-app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(express.static(process.env.BUILD_DIR || 'frontend/build'))	// Relative to node process.
 
-app.use(express.static('../frontend/build'))
 app.use('/api/assembler', assemblerRouter)
 
 app.use(middleware.unknownEndpoint)
--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -1,16 +1,17 @@
 const logger = require('./logger')
 
-const requestLogger = (request, response, next) => {
+const requestLogger = (req, res, next) => {
 	logger.info('Time:	', new Date().toString())
-	logger.info('Method:', request.method)
-	logger.info('Path:	', request.path)
-	logger.info('Body:	', request.body)
+	logger.info('Method:', req.method)
+	logger.info('Path:	', req.path)
+	logger.info('Body:	', req.body)
+	logger.info('IP:	', req.ip)
 	logger.info('---')
 	next()
 }
 
-const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: 'unknown endpoint' })
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: 'unknown endpoint' })
 }
 
 module.exports = {
