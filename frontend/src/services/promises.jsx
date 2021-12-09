35d7e9525acdb99564e44be81422ed692c9cc9bc
Deep Sharma <deepsharma.cs@gmail.com>
2021-12-09 22:44:00 +0530

    rename promises.js to promises.jsx

--- frontend/src/services/promises.jsx
+++ frontend/src/services/promises.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 const baseUrl = '/api/assembler'
 
 const sendCode = code => {
+
 	const request = axios.post(baseUrl, {code})
 	return request.then(response => response.data)
 }
